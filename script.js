e5e70927e98fb8b981091d32229b8fc3486d3707
Shabrina <shabrina.m13@gmail.com>
2025-07-03 13:32:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-<script>
-    const form = document.getElementById("todo-form");
-    const input = document.getElementById("todo-input");
-    const list = document.getElementById("todo-list");
+const form = document.getElementById("todo-form");
+const input = document.getElementById("todo-input");
+const list = document.getElementById("todo-list");
     
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-        const newItemText = input.value.trim();
-        if (newItemText !== "") {
-          const li = document.createElement("li");
-          li.textContent = newItemText;
-          list.appendChild(li);
-          input.value = "";
-        }
-    })
-</script>
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const newItemText = input.value.trim();
+    if (newItemText !== "") {
+        const li = document.createElement("li");
+        li.textContent = newItemText;
+        list.appendChild(li);
+        input.value = "";
+    }
+})
 
 console.log("Script is working!");
