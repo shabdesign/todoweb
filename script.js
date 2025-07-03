de911da13b9d64eaf2ce3d72772ddb426bc83751
Shabrina <shabrina.m13@gmail.com>
2025-07-03 13:28:37 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+<script>
+    const form = document.getElementById("todo-form");
+    const input = document.getElementById("todo-input");
+    const list = document.getElementById("todo-list");
+    
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const newItemText = input.value.trim();
+        if (newItemText !== "") {
+          const li = document.createElement("li");
+          li.textContent = newItemText;
+          list.appendChild(li);
+          input.value = "";
+        }
+    })
+</script>
+
+console.log("Script is working!");
