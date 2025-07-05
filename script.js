569e26885d260c6e802fb2fd27f1b408c3b471f1
Shabrina <shabrina.m13@gmail.com>
2025-07-05 20:12:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,38 +11,36 @@ document.addEventListener("DOMContentLoaded", function () {
       li.classList.add("todo-item");
       const spanText = document.createElement("span");
       spanText.textContent = newItemText;
-        saveListToLocalStorage();
       const deleteBtn = document.createElement("button");
       deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
       deleteBtn.classList.add("delete-btn");
       deleteBtn.addEventListener("click", function () {
         li.remove();
-        saveListToLocalStorage();
       });
       li.addEventListener("dblclick", function () {
         li.classList.toggle("completed");
-        saveListToLocalStorage();
       });
       li.appendChild(spanText);
       li.appendChild(deleteBtn);
       list.appendChild(li);
       input.value = "";
+      saveListToLocalStorage();
     const clearCompletedBtn = document.getElementById("clear-completed");
     clearCompletedBtn.addEventListener("click", function () {
       const completedItems = document.querySelectorAll("#todo-list .completed");
       completedItems.forEach(function (item) {
         item.remove();
       });
-      saveListToLocalStorage();
     });
+      saveListToLocalStorage();
     const emptyListBtn = document.getElementById("empty-list");
     emptyListBtn.addEventListener("click", function () {
       const allItems = document.querySelectorAll("#todo-list li");
       allItems.forEach(function (item) {
         item.remove();
-        saveListToLocalStorage();
       });
     });
+      saveListToLocalStorage();
     }
   });
   function saveListToLocalStorage() {
